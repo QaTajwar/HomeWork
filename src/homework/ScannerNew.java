4e4e63f3310dd4de78a597d284a7f00d09d159eb
QaTajwar <103096244+QaTajwar@users.noreply.github.com>
2022-04-15 23:19:07 -0400

    Updated Home Work

---
+++ src/homework/ScannerNew.java
@@ -0,0 +1,34 @@
+package homework;
+
+import java.util.Scanner;
+
+public class ScannerNew {
+
+
+    public void doSummation(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int num1 = input.nextInt();
+        System.out.println("Enter the second number");
+        int num2 = input.nextInt();
+        int total = num1+num2;
+        System.out.println("Total Summation : "+total);
+
+    }
+    public void doSubtraction(){
+        Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        int total = num1-num2;
+        System.out.println("Total Subtraction : "+total);
+
+    }
+
+    public static void main(String[] args) {
+        ScannerNew hi = new ScannerNew();
+        hi.doSummation();
+        hi.doSubtraction();
+
+
+    }
+}
